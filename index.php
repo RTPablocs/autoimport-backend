202bfc020012f6dd80bdd7aac32e9160df69cefe
Pablo Conejero <conejerosorianopablo@gmail.com>
2021-01-20 02:36:42 +0100

    added db_controller and rewrited 1st get route

--- index.php
+++ index.php
@@ -4,11 +4,13 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+require 'database/db_controller.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args){
-    $response->getBody()->write("Hello World");
+$app->get('/api/getAllMakes()', function (Request $request, Response $response, $args){
+    $action = getAllMakes();
+    $response->getBody()->write($action);
     return $response;
 });
 
