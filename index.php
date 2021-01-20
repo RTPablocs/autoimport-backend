8f320eab0fcd58909029a607764f307982b95f3b
Pablo Conejero <conejerosorianopablo@gmail.com>
2021-01-20 02:54:15 +0100

    added new get route

--- index.php
+++ index.php
@@ -8,8 +8,15 @@ require 'database/db_controller.php';
 
 $app = AppFactory::create();
 
-$app->get('/api/getAllMakes()', function (Request $request, Response $response, $args){
+$app->get('/api/getAllMakes', function (Request $request, Response $response, $args){
     $action = getAllMakes();
+    $response->getBody()->write($action);
+    return $response;
+});
+
+$app->get('/api/make/{makeId}', function (Request $request, Response $response, $args){
+    $makeId = $args['makeId'];
+    $action = getMakeId($makeId);
     $response->getBody()->write($action);
     return $response;
 });
