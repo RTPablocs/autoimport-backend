6ac2bf9b13cdd7c3a18a463863eb666f4a08bcd9
Pablo Conejero <conejerosorianopablo@gmail.com>
2021-01-22 15:33:21 +0100

    added header for /api/getAllMakes

--- index.php
+++ index.php
@@ -4,12 +4,13 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
-require 'database/db_controller.php';
+require 'database/db_car_controller.php';
 
 $app = AppFactory::create();
 
 $app->get('/api/getAllMakes', function (Request $request, Response $response, $args){
     $action = getAllMakes();
+    $response->withHeader('Content-type', 'application/json');
     $response->getBody()->write($action);
     return $response;
 });
