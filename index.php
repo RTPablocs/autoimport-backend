d25efb040a13f98a385cd07351fe3fff56220f15
RTPablocs <conejerosorianopablo@gmail.com>
2021-01-25 19:46:00 +0100

    removed routes from index, as now are grouped

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
-require 'database/db_car_controller.php';
 
 $app = AppFactory::create();
 
-$app->get('/api/getAllMakes', function (Request $request, Response $response, $args){
-    $action = getAllMakes();
-    $response->withHeader('Content-type', 'application/json');
-    $response->getBody()->write($action);
-    return $response;
-});
-
-$app->get('/api/make/{makeId}', function (Request $request, Response $response, $args){
-    $makeId = $args['makeId'];
-    $action = getMakeId($makeId);
-    $response->getBody()->write($action);
-    return $response;
-});
+require __DIR__ . '/routes/routes.php';
 
 $app->run();
