fe2a578642e2f6ba30baec8f3c3a9f7df266f812
RTPablocs <conejerosorianopablo@gmail.com>
2021-01-28 17:56:39 +0100

    Deleted Redundant files

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$db_options = parse_ini_file('access.ini');
-
-define('HOST', $db_options['host']);
-define('USER', $db_options['user']);
-define('PASSWORD', $db_options['password']);
-define('DB', $db_options['db']);
