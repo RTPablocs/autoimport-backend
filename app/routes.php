86fd7f936e26c58baa31d9df5ff81f562958f6be
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-01 11:55:58 +0100

    Created new routes for the app

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+use App\Application\Actions\Ad\ListAdsAction;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -22,5 +23,15 @@ return function (App $app) {
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
         $group->get('/{id}', ViewUserAction::class);
+        //$group->post('/create');
+        //$group->delete('/delete/{id}');
+    });
+
+    $app->group('/ads', function (Group $group){
+       $group->get('', ListAdsAction::class);
+       //$group->get('/{id}');
+       //$group->delete('/delete/{id}');
+       //$group->post('/new/{id}');
+       //$group->patch('/update/{id}');
     });
 };
