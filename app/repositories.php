6cb7ff1ce4a086c486bb34c35ba70e102072b9c2
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-01 11:55:35 +0100

    added AdRepository with Autowire

--- app/repositories.php
+++ app/repositories.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 
 use App\Domain\User\UserRepository;
+use App\Domain\Ad\AdRepository;
+use App\Infrastructure\Persistence\Ad\PersistentAdRepository;
 use App\Infrastructure\Persistence\User\InMemoryUserRepository;
 use DI\ContainerBuilder;
 
@@ -9,5 +11,7 @@ return function (ContainerBuilder $containerBuilder) {
     // Here we map our UserRepository interface to its in memory implementation
     $containerBuilder->addDefinitions([
         UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
+        AdRepository::class => \DI\autowire(PersistentAdRepository::class)
+
     ]);
 };
