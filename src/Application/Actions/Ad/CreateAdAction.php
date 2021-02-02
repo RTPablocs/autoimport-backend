5583cc12bd75cf304d7be0de1bb2945bf3e1b85e
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-02 21:02:25 +0100

    Created CreateAdAction for handling ad creation

---
+++ src/Application/Actions/Ad/CreateAdAction.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Application\Actions\Ad;
+
+use App\Application\Actions\ActionPayload;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use App\Application\Actions\Ad\AdAction;
+
+class CreateAdAction extends AdAction{
+  
+
+  protected function action(): Response{  
+
+    $payload = new ActionPayload(215, 'Ad Created');
+    $json = $this->request->getBody();
+    $data = json_decode($json, true);
+    
+    $this->adRepository->createOne($data);
+
+    $this->logger->info('Ad Created ');
+    return $this->respond($payload);
+  }
+}
+
