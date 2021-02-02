5aeb32136a4ec2de6862ffcc17dad01996bcb30e
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-02 21:01:22 +0100

    added deleteOne and createOne methods

--- src/Infrastructure/Persistence/Ad/PersistentAdRepository.php
+++ src/Infrastructure/Persistence/Ad/PersistentAdRepository.php
@@ -22,10 +22,31 @@ class PersistentAdRepository implements AdRepository {
 
     public function findAdOfMake($makeName, $price): array {
         // TODO: Implement findAdOfMake() method.
+        return array();
     }
 
     public function findAdOfModel($modelName, $price): array {
         // TODO: Implement findAdOfModel() method.
+        return array();
     }
 
+    public function deleteOne($adId): void {
+        $sql = "delete from ads where id = $adId;";
+        $this->connection->prepare($sql)->execute();
+    }
+
+    public function createOne($data): void {
+        
+
+        $id = (int) $data['id'];
+        $title = $data['title'];
+        $desc = $data['desc'];
+        $make = $data['makeName'];
+        $model = $data['modelName'];
+        $price = $data['price'];
+        $coords = (string) $data['coords'];
+
+        $sql = "insert into ads values ($id, '$title', '$desc', '$make', '$model', $price, '$coords');";
+        $this->connection->prepare($sql)->execute();
+    }
 }
