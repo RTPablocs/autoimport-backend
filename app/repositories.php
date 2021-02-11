0c4a6b9902a70dabc0d20c5f32cab24fdaf87d3d
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-11 17:56:55 +0100

    Added CategoryRepository to autowire

--- app/repositories.php
+++ app/repositories.php
@@ -3,15 +3,17 @@ declare(strict_types=1);
 
 use App\Domain\User\UserRepository;
 use App\Domain\Ad\AdRepository;
+use App\Domain\Category\CategoryRepository;
 use App\Infrastructure\Persistence\Ad\PersistentAdRepository;
 use App\Infrastructure\Persistence\User\InMemoryUserRepository;
+use App\Infrastructure\Persistence\Category\PersistentCategoryRepository;
 use DI\ContainerBuilder;
 
 return function (ContainerBuilder $containerBuilder) {
     // Here we map our UserRepository interface to its in memory implementation
     $containerBuilder->addDefinitions([
         UserRepository::class => \DI\autowire(InMemoryUserRepository::class),
-        AdRepository::class => \DI\autowire(PersistentAdRepository::class)
-
+        AdRepository::class => \DI\autowire(PersistentAdRepository::class),
+        CategoryRepository::class => \DI\autowire(PersistentCategoryRepository::class)
     ]);
 };
