1cfe516d598ea9529aad5b5670c7c6b25afb3b6e
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-11 17:57:22 +0100

    Added Routes for Categories

--- app/routes.php
+++ app/routes.php
@@ -1,11 +1,21 @@
 <?php
 declare(strict_types=1);
 
+//User Actions
 use App\Application\Actions\User\ListUsersAction;
 use App\Application\Actions\User\ViewUserAction;
+
+//Ad Actions
 use App\Application\Actions\Ad\ListAdsAction;
 use App\Application\Actions\Ad\DeleteAdAction;
 use App\Application\Actions\Ad\CreateAdAction;
+use App\Application\Actions\Ad\ViewAdAction;
+use App\Application\Actions\Ad\UpdateAdAction;
+
+//Category Actions
+use App\Application\Actions\Category\ListCategoriesAction;
+
+//Slim Dependencies
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
@@ -31,9 +41,10 @@ return function (App $app) {
 
     $app->group('/ads', function (Group $group){
        $group->get('', ListAdsAction::class);
-       //$group->get('/{id}');
+       $group->get('/{id}', ViewAdAction::class);
        $group->delete('/delete/{id}', DeleteAdAction::class);
        $group->post('/new', CreateAdAction::class);
-       //$group->patch('/update/{id}');
+       $group->patch('/{id}', UpdateAdAction::class);
     });
+    $app->get('/categories', ListCategoriesAction::class);
 };
