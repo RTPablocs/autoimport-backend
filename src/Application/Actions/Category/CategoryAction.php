7b7fa4352c6ccf9f08557a1ca5bb50b8ab1d59f6
RTPablocs <conejerosorianopablo@gmail.com>
2021-02-11 18:00:01 +0100

    Added CategoryAction

---
+++ src/Application/Actions/Category/CategoryAction.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Application\Actions\Category;
+
+use App\Application\Actions\Action;
+use App\Domain\Category\CategoryRepository;
+use Psr\Log\LoggerInterface;
+
+abstract class CategoryAction extends Action{
+    protected $adRepository;
+
+    public function __construct(LoggerInterface $logger, CategoryRepository $categoryRepository){
+        parent::__construct($logger);
+        $this->categoryRepository = $categoryRepository;
+    }
+}
